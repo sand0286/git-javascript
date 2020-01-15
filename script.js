adb894867bae9eaef968766a07223171bb4e5892
Smiledeep sandhu <sand0286@algonquinlive.com>
2020-01-15 10:16:16 -0500

    adding all files

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = 'smile'
+console.log('Hello, my name is $(name).')
+
+Number = 5
+
+console.log('the number ${number} multiplied by 2 is ${number * 2}.')
+
+console.log('the number multiplied  by 5 is $ {product * 5}.')
+console.log('the result divided by 5 is ${(product * 5) / number}')
+let finvalue = (product * 5) / number
+console.log('the result minus 7 is ${finvalue - 7}.')
